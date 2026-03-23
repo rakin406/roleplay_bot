c3674891f1df8e600c27ebbfe2b8281f0cb5f417
Rakin Rahman <rakinhacker@gmail.com>
2026-03-24 00:06:40 +0600

    feat: throw error if input invalid

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 """This is the CLI program of the roleplay bot."""
 
+import sys
+
+
+def validate(message: str) -> str:
+    if not message:
+        raise ValueError("String cannot be empty")
+    return message
+
 
 def main():
     """Main function."""
-    print("Hello from roleplay-bot!")
+    try:
+        bot_name = validate(input("Bot's nickname: "))
+        username = validate(input("Your nickname: "))
+    except ValueError:
+        print("Invalid input")
+        sys.exit(1)
+
+    bot_description = input("Bot's character description (Optional): ")
+    user_description = input("Your character description (Optional): ")
+    world = input("Scenario and lore (Optional): ")
 
 
 if __name__ == "__main__":
