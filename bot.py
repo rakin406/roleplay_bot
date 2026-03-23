71ff4703dfcfd8fc01326efdc859fff6341612f9
Rakin Rahman <rakinhacker@gmail.com>
2026-03-24 00:28:35 +0600

    feat: add Bot and BotParameters classes

--- bot.py
+++ bot.py
@@ -1,6 +1,21 @@
 """This module contains the AI chatbot."""
 
+from dataclasses import dataclass
+
+
+@dataclass
+class BotParameters:
+    """Class for storing bot information."""
+
+    bot_name: str
+    anon_name: str
+    bot_description: str | None
+    anon_description: str | None
+    world: str | None  # Scenario and lore
+
 
 class Bot:
-    def __init__(self):
-        pass
+    """Class for running the chatbot."""
+
+    def __init__(self, params: BotParameters):
+        self.params = params
